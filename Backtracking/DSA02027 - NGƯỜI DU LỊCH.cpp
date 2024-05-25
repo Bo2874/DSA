8f8f7de6e71a7913ae3138a6641a8723242a87df
Bo2874 <164356654+Bo2874@users.noreply.github.com>
2024-05-25 21:39:33 +0700

    Update DSA02027 - NGƯỜI DU LỊCH.cpp

--- Backtracking/DSA02027 - NGƯỜI DU LỊCH.cpp
+++ Backtracking/DSA02027 - NGƯỜI DU LỊCH.cpp
@@ -1,18 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, X[100], C[100][100], ans = 1e9, cmin = 1e9;
+int n, X[100], C[100][100], ans = 1e9;
 bool visited[100];
 void Try(int i, int sum){
 	for(int j = 2; j <= n ; j++){
 		if(visited[j] == false){
 		visited[j] = true;
 		X[i] = j;
-		sum += C[X[i-1]][X[i]];
-		if(i == n) ans = min(ans, sum + C[X[n]][1]);
-		else if(sum + (n-i)*cmin < ans) Try(i+1, sum);
+		if(i == n) ans = min(ans, sum + C[X[i-1]][X[i]] + C[X[i]][1]); // cộng thêm 1 lần từ đỉnh cuối vòng về 1
+		else if(sum + C[X[i-1]][X[i]] < ans) Try(i+1, sum + C[X[i-1]][X[i]]);
+	    // nếu cái kq này bé hơn kq trước đó thì mới thử tiếp
 		visited[j] = false;
-		sum -= C[X[i-1]][X[i]];
 		}
 	}
 }
@@ -22,7 +21,6 @@ int main(){
 	for(int i = 1; i <= n ;i++){
 		for(int j = 1; j <= n ; j++){
 			cin >> C[i][j];
-			if(C[i][j]) cmin = min(cmin, C[i][j]);
 		}
 	}
 	X[1] = 1;
